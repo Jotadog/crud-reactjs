6c66b039a7c988f6b31ee28c75338a2bf6598615
Jotadog <jvscaravonatti@gmail.com>
2019-01-22 17:14:47 -0200

    Mensagem de registro OK

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -7,33 +7,50 @@ class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            firstName: '',
-            lastName: '',
-            honorific: 'Mr',
-            email: '',
-            phone: '',
-            password: '',
-            confirmPassword: ''
+            data: {
+                firstName: '',
+                lastName: '',
+                honorific: 'Mr',
+                email: '',
+                phone: '',
+                password: '',
+                confirmPassword: '',
+            },
+            message: false
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    addMessage = () => {
+        this.setState({ message: true });
+    }
+
+    removeMessage = () => {
+        setTimeout(function () {
+            this.setState({ message: false });
+        }.bind(this), 2000);
+    }
+
     handleChange(event) {
         this.setState({
-            [event.target.name]: event.target.value
+            data: {
+                [event.target.name]: event.target.value
+            }
         });
     }
 
     store = () => {
-        Axios.post('http://localhost:3001/api/profiles', this.state)
+        Axios.post('http://localhost:3001/api/profiles', this.state.data)
             .catch(console.log);
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        this.addMessage();
         this.store();
+        this.removeMessage();
     }
 
     render() {
@@ -55,35 +72,44 @@ class Register extends Component {
                             </div>
                             <div className="form-group col-5">
                                 <label htmlFor="firstName">First name</label>
-                                <input type="text" className="form-control" id="firstName" name='firstName' value={this.state.firstName} placeholder="First name" onChange={this.handleChange} />
+                                <input type="text" className="form-control" id="firstName" name='firstName' value={this.state.data.firstName} placeholder="First name" onChange={this.handleChange} />
                             </div>
                             <div className="form-group col-5">
                                 <label htmlFor="lastName">Last name</label>
-                                <input type="text" className="form-control" id="lastName" name="lastName" value={this.state.lastName} placeholder="Last name" onChange={this.handleChange} />
+                                <input type="text" className="form-control" id="lastName" name="lastName" value={this.state.data.lastName} placeholder="Last name" onChange={this.handleChange} />
                             </div>
                         </div>
                         <div className='row'>
                             <div className='form-group col 6'>
                                 <label htmlFor='email'>E-mail</label>
-                                <input type="email" className="form-control" id="email" name="email" value={this.state.email} placeholder="E-mail" onChange={this.handleChange} />
+                                <input type="email" className="form-control" id="email" name="email" value={this.state.data.email} placeholder="E-mail" onChange={this.handleChange} />
                             </div>
                             <div className='form-group col 6'>
                                 <label htmlFor='phone'>Phone</label>
-                                <input type="text" className="form-control" id="phone" name="phone" value={this.state.phone} placeholder="Phone" onChange={this.handleChange} />
+                                <input type="text" className="form-control" id="phone" name="phone" value={this.state.data.phone} placeholder="Phone" onChange={this.handleChange} />
                             </div>
                         </div>
                         <div className='row'>
                             <div className='form-group col 6'>
                                 <label htmlFor='password'>Password</label>
-                                <input type="password" className="form-control" id="password" name="password" value={this.state.password} placeholder="Password" onChange={this.handleChange} />
+                                <input type="password" className="form-control" id="password" name="password" value={this.state.data.password} placeholder="Password" onChange={this.handleChange} />
                             </div>
                             <div className='form-group col 6'>
                                 <label htmlFor='confirmPassword'>Confirm password</label>
-                                <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={this.state.confirmPassword} placeholder="Confirm password" onChange={this.handleChange} />
+                                <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={this.state.data.confirmPassword} placeholder="Confirm password" onChange={this.handleChange} />
                             </div>
                         </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form>
+                    {
+                        this.state.message ?
+                            <Fragment>
+                                <br />
+                                <h5 className='text-success'>Registered</h5>
+                            </Fragment>
+                            :
+                            <div></div>
+                    }
                 </div>
             </Fragment >
         );
